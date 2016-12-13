5a4a433113e577d52cb73cb38c0f8db421ea2bad
Romolo Marotta <romolo.marotta@gmail.com>
2016-12-13 14:53:28 +0100

    added release makefile

---
+++ Release/src/datatypes/calqueue.d
@@ -0,0 +1,6 @@
+src/datatypes/calqueue.o: ../src/datatypes/calqueue.c \
+ ../src/datatypes/calqueue.h ../src/datatypes/../arch/atomic.h
+
+../src/datatypes/calqueue.h:
+
+../src/datatypes/../arch/atomic.h:
