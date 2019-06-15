c8ce70b197c7fc9b0b170b4c326a7887387df06d
Romolo Marotta <romolo.marotta@gmail.com>
2019-06-15 16:25:08 +0200

    Removing unused files

--- Release/src/datatypes/nb_calqueue.d
+++
@@ -1,9 +0,0 @@
-src/datatypes/nb_calqueue.o: ../src/datatypes/nb_calqueue.c \
- ../src/datatypes/nb_calqueue.h ../src/datatypes/../arch/atomic.h \
- ../src/datatypes/../utils/util.h
-
-../src/datatypes/nb_calqueue.h:
-
-../src/datatypes/../arch/atomic.h:
-
-../src/datatypes/../utils/util.h:
