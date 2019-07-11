d27ee91484abf86a1252f42f0c24e05aaafffcf6
Romolo <marotta@diag.uniroma1.it>
2019-07-11 18:53:11 +0200

    Refactor of rtm.h

--- src/utils/rtm.h
+++ src/utils/rtm.h
@@ -5,18 +5,19 @@
 
 #define RTM
 
+extern __thread unsigned long long rtm_prova, rtm_failed, rtm_insertions, insertions;
 #ifdef RTM
 
 #define DEB(x) {}
 //#define DEB(x) printf(x)
 
 #define TM_COMMIT() _xend()
-#define TM_ABORT()  _xabort(0)
+#define TM_ABORT()  _xabort(0xff)
 
 #define ATOMIC  CMB();\
-{	++prova;/*printf("Transactions %u, %u, %u\n", prova, failed, insertions);*/\
+{	++rtm_prova;/*printf("Transactions %u, %u, %u\n", prova, failed, insertions);*/\
 	unsigned int __status = 0;\
-	retry_tm:\
+	/*retry_tm:*/\
 	if ((__status = _xbegin ()) == _XBEGIN_STARTED)
 	//{
 	//}
@@ -36,7 +37,7 @@
 		else if(__status & _XABORT_NESTED) {DEB("NESTES\n");}\
 		/*  Transaction explicitely aborted with _xabort. */\
 		else if(__status & _XABORT_EXPLICIT){DEB("EXPLICIT\n");}\
-		else{DEB("Other\n");failed++;}\
+		else{DEB("Other\n");rtm_failed++;}\
 			//{
 			//}
 
@@ -48,13 +49,33 @@
 
 #else
 
+#include <pthread.h>
+pthread_mutex_t glock = PTHREAD_MUTEX_INITIALIZER; 
+
 #define TM_COMMIT() /* */
 #define TM_ABORT()  {aborted=1;break;}
 
-#define BEGIN_ATOMIC()   {unsigned int aborted=0;	do{
-#define FALLBACK() }while(0);if(aborted){{printf("EXPLICIT\n");}
+#define ATOMIC   \
+{	++rtm_prova;\
+	unsigned int aborted=0;\
+	do{\
+		pthread_mutex_lock(&glock);
+		//{
+		//}
+	
+#define FALLBACK() \
+	}while(0);\
+	if(aborted){\
+		rtm_failed++;\
+		pthread_mutex_unlock(&glock); \
+		/*printf("EXPLICIT\n");*/
+		//{
+		//}
 
-#define END_ATOMIC() }}
+#define END_ATOMIC() \
+	}\
+	pthread_mutex_unlock(&glock);\
+}
 
 #endif
 
