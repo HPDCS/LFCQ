872410c415cabba6a8707b1ac6938f8ac92f5a6c
Romolo Marotta <romolo.marotta@gmail.com>
2019-07-25 14:19:43 +0200

    Debugging tails

--- src/datatypes/nbcalendars_with_vb2/set_table.h
+++ src/datatypes/nbcalendars_with_vb2/set_table.h
@@ -723,7 +723,7 @@ int  migrate_node(bucket_t *bckt, table_t *new_h)
 			while(ln->timestamp != INFTY)
 				ln = ln->next;
 
-			if(ln == INFTY) assert(ln == left->tail);
+			if(ln->timestamp == INFTY) assert(ln == left->tail);
 
 			flush_current(new_h, new_index);
 			curr = head->next;
