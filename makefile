df31c1b82114162c8d59b9cfac59de8fc36bbc6d
Umberto Mazziotta <umberto961@live.it>
2019-11-14 16:06:09 +0100

    Test for SC version of NUMAP

--- makefile
+++ makefile
@@ -10,7 +10,7 @@ USER_OBJS :=
 
 LIBS := -lpthread -lm -lnuma -lrt -mrtm
 SRC_DIR := src
-TARGETS := ACRCQ NUMAP RNUMAP NUMAPFK NUMAPBL NUMAPSKT RNUMAPSKT NUMAPSKTFK NUMAPSKTBL NUMAQ RNUMAQ NUMAQFK NUMAQBL NUMAQSKT RNUMAQSKT NUMAQSKTFK NUMAQSKTBL NUMA2Q RNUMA2Q NUMA2QFK NUMA2QBL NUMA2QSKT RNUMA2QSKT NUMA2QSKTFK NUMA2QSKTBL CACRCQ
+TARGETS := ACRCQ NUMAP RNUMAP NUMAPFK NUMAPBL NUMAPSKT RNUMAPSKT NUMAPSKTFK NUMAPSKTBL NUMAQ RNUMAQ NUMAQFK NUMAQBL NUMAQSKT RNUMAQSKT NUMAQSKTFK NUMAQSKTBL NUMA2Q RNUMA2Q NUMA2QFK NUMA2QBL NUMA2QSKT RNUMA2QSKT NUMA2QSKTFK NUMA2QSKTBL CACRCQ SCNUMAP
 		#NUMAQ NUMAQBLK NUMAFK NUMAFKBL NUMAP NUMAPNOP NUMAPBL NUMAPSKT NUMAPSKTFK NUMAPSKTBL NUMAPNBCSKT NUMAPNBCSKTFK NUMAPNBCSKTBL # NUMAPNBC NUMAPNBCFK NUMAPNBCBL NUMAPFNB NUMA2Q NUMA2QFK NUMA2QBL NUMA2QBLK # #NBCQ LIND MARO CBCQ SLCQ NBVB 2CAS VBPQ ACRCQ #V2CQ NUMA WORK
 		# NUMA2Q no cache to see difference + ACRCQH + Cache to other structures
 UTIL_value := src/utils/common.o src/utils/hpdcs_math.o 
@@ -172,6 +172,9 @@ NUMA2QSKTBL_link := gcc
 # OTHER DATA
 
 ######
+
+SCNUMAP_value := src/datatypes/nb_umb/numap_SC/base.o src/datatypes/nb_umb/numap_SC/mapping.o $(UTIL_value) $(ARCH_value) $(NGACO_value)
+SCNUMAP_link := gcc
 
 CACRCQ_value := src/datatypes/nb_umb/nbcalendars-ad-candidate/nb_calqueue.o src/datatypes/nb_umb/nbcalendars-ad-candidate/common_nb_calqueue.o $(UTIL_value) $(ARCH_value) $(NGACO_value)
 CACRCQ_link := gcc
@@ -378,7 +381,7 @@ endif
 -funswitch-loops \
 -fvect-cost-model 
 
-C_SUBDIRS		:= src src/datatypes/nbcalendars-ad src/datatypes/nbcalendars src/datatypes/nbcalendars_with_vb src/datatypes/nbcalendars_with_vb2  src/datatypes/nbcalendars_with_vb_2CAS  src/datatypes/nbskiplists src/datatypes/slcalqueue  src/arch src/gc src/utils src/datatypes/nb_umb/op_queue src/datatypes/nb_umb/gc src/datatypes/nb_umb/unbcq src/datatypes/nb_umb/numa2q src/datatypes/nb_umb/numa2q_socket src/datatypes/nb_umb/numap src/datatypes/nb_umb/numap_socket src/datatypes/nb_umb/numaq src/datatypes/nb_umb/numaq_socket src/datatypes/nb_umb/nbcq_phy src/datatypes/nb_umb/numap_fnb src/datatypes/nb_umb/numaq_no_candidate src/datatypes/nb_umb/numaq_no_candidate_socket src/datatypes/nb_umb/numa2q_no_candidate src/datatypes/nb_umb/numa2q_no_candidate_socket src/datatypes/nb_umb/nbcalendars-ad-candidate
+C_SUBDIRS		:= src src/datatypes/nbcalendars-ad src/datatypes/nbcalendars src/datatypes/nbcalendars_with_vb src/datatypes/nbcalendars_with_vb2  src/datatypes/nbcalendars_with_vb_2CAS  src/datatypes/nbskiplists src/datatypes/slcalqueue  src/arch src/gc src/utils src/datatypes/nb_umb/op_queue src/datatypes/nb_umb/gc src/datatypes/nb_umb/unbcq src/datatypes/nb_umb/numa2q src/datatypes/nb_umb/numa2q_socket src/datatypes/nb_umb/numap src/datatypes/nb_umb/numap_socket src/datatypes/nb_umb/numaq src/datatypes/nb_umb/numaq_socket src/datatypes/nb_umb/nbcq_phy src/datatypes/nb_umb/numap_fnb src/datatypes/nb_umb/numaq_no_candidate src/datatypes/nb_umb/numaq_no_candidate_socket src/datatypes/nb_umb/numa2q_no_candidate src/datatypes/nb_umb/numa2q_no_candidate_socket src/datatypes/nb_umb/nbcalendars-ad-candidate src/datatypes/nb_umb/numap_SC
 
 C_SRCS			:= $(shell ls   $(patsubst %, %/*.c, $(C_SUBDIRS)) )
 C_SRCS 			:= $(filter-out $(FILTER_OUT_C_SRC), $(C_SRCS))
---
+++ src/datatypes/nb_umb/numap_SC/mapping.c
@@ -0,0 +1,161 @@
+#include <numa.h>
+#include <numaif.h>
+
+#include "mapping.h"
+
+#ifndef _NM_USE_SPINLOCK
+#define R_BUSY 0x2
+#define W_BUSY 0x1
+#define L_FREE 0x0
+#endif
+
+struct __op_load
+{
+	#ifdef _NM_USE_SPINLOCK
+	spinlock_t spin;
+	#else
+	volatile int busy;
+	#endif
+	volatile int response; 		// 0 posted/wait to be executed; >=1 read/executed;
+	
+	char pad0[56];
+
+    unsigned int dest_node;
+	unsigned int type;			// ENQ | DEQ
+	int ret_value;
+	pkey_t timestamp;			// ts of node to enqueue
+ 	void *payload;				// paylod to enqueue | dequeued payload
+	//24
+	char pad[48 - sizeof(pkey_t) ];
+
+};
+
+op_node *res_mapping[_NUMA_NODES];
+op_node *req_mapping[_NUMA_NODES];
+
+void init_mapping() 
+{
+    unsigned int max_threads = _NUMA_NODES * num_cpus_per_node;
+    int i, j;
+    for (i = 0; i < _NUMA_NODES; ++i) 
+    {
+        req_mapping[i] = numa_alloc_onnode(sizeof(op_node)*max_threads, i);
+        res_mapping[i] = numa_alloc_onnode(sizeof(op_node)*max_threads, i);
+        
+        for (j = 0; j < max_threads; ++j) 
+        {
+            #ifdef _NM_USE_SPINLOCK
+            spinlock_init(&(req_mapping[i][j].spin));
+            spinlock_init(&(res_mapping[i][j].spin));
+            #else
+            req_mapping[i][j].busy = L_FREE;
+            res_mapping[i][j].busy = L_FREE;
+            #endif
+
+            res_mapping[i][j].response = 1;
+            req_mapping[i][j].response = 1;
+        }
+    }
+    LOG("init_mapping() done! %s\n","(mapping)");
+}
+
+op_node* get_req_slot_from_node(unsigned int numa_node)
+{
+    return &req_mapping[NID][(numa_node * num_cpus_per_node) + LTID];
+}
+
+op_node* get_req_slot_to_node(unsigned int numa_node)
+{
+    return &req_mapping[numa_node][TID];
+}
+
+op_node* get_req_slot_from_to_node(unsigned int src_node, unsigned int dst_node)
+{
+    return &req_mapping[dst_node][(src_node * num_cpus_per_node) + LTID];
+}
+
+op_node* get_res_slot_from_node(unsigned int numa_node)
+{
+    return &res_mapping[NID][(numa_node * num_cpus_per_node) + LTID];
+}
+
+op_node* get_res_slot_to_node(unsigned int numa_node)
+{
+    return &res_mapping[numa_node][TID];
+}
+
+bool read_slot(op_node* slot, 
+    unsigned int* type, 
+    int *ret_value, 
+    pkey_t *timestamp, 
+    void** payload,
+    unsigned int *node) 
+{
+    
+    int val;
+
+    #ifdef _NM_USE_SPINLOCK
+    if (!spin_trylock_x86(&(slot->spin)))
+        return false;
+    #else
+    if (!__sync_bool_compare_and_swap(&(slot->busy), L_FREE, R_BUSY))
+        return false;
+    #endif
+
+    val = __sync_fetch_and_add(&(slot->response), 1);
+    if (val != 0)
+    {
+        slot->busy = L_FREE;
+        return false;
+    }
+
+    *node = slot->dest_node;
+    *type = slot->type;
+    *ret_value = slot->ret_value;
+    *timestamp = slot->timestamp; 
+    *payload = slot->payload;
+    
+    #ifdef _NM_USE_SPINLOCK
+    spin_unlock_x86(&(slot->spin));
+    #else
+    slot->busy = L_FREE;
+    #endif
+
+    return true;
+}
+
+bool write_slot(op_node* slot, 
+    unsigned int type, 
+    int ret_value, 
+    pkey_t timestamp, 
+    void* payload,
+    unsigned int node)
+{
+
+    int val;
+
+    #ifdef _NM_USE_SPINLOCK
+    spin_lock_x86(&(slot->spin));
+    #else
+    while(!__sync_bool_compare_and_swap(&(slot->busy), L_FREE, W_BUSY));
+    #endif
+
+    slot->dest_node = node;
+    slot->type = type;
+    slot->ret_value = ret_value;
+    slot->timestamp = timestamp; 
+    slot->payload = payload;
+
+    val = __sync_fetch_and_and(&(slot->response), 0);
+
+    #ifdef _NM_USE_SPINLOCK
+    spin_unlock_x86(&(slot->spin));
+    #else
+    slot->busy = L_FREE;
+    #endif
+    if (val != 0)
+        return true;
+    else 
+        return false;
+}
+
